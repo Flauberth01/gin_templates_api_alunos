17f5299718f5da245d503c0b8bd768399c8d717e
flauberth01 <flauberth12@gmail.com>
2024-07-15 13:37:01 -0500

    refactor: remover comentários desnecessários no main.go

--- main.go
+++ main.go
@@ -1,11 +1,5 @@
 package main
 
-/* Modo de uso:
-
-1. Criar um aluno atráves do postman (POST)
-
-*/
-
 import (
 	"github.com/flauberth01/gin_templates_api_alunos/database"
 	"github.com/flauberth01/gin_templates_api_alunos/routes"
